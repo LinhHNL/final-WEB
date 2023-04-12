dba88a27f941f4345058dbf18804b0d592e787e4
Ngo Chi Cuong <saofaixoan5464@gmail.com>
2023-04-12 23:22:54 +0700

    Adding API homepage 80%

--- View/API/MovieAPI.js
+++ View/API/MovieAPI.js
@@ -1,6 +1,17 @@
-const getHotMovieAPI = async () => {
+const getHotMovieAPI = async (url) => {
   const data = await fetch(
-    "../../Controller/Movie/MoiveService.php?action=getMoiveHot",
+    `${url}/Controller/Movie/MoiveService.php?action=getMoiveHot`,
+    {
+      method: "GET",
+    }
+  );
+  const datatorender = await data.json();
+
+  return datatorender;
+};
+const getPremierMovie = async (url, page = 1) => {
+  const data = await fetch(
+    `${url}/Controller/Movie/MoiveService.php?action=getPremieredMovies&page=${page}`,
     {
       method: "GET",
     }
@@ -8,21 +19,9 @@ const getHotMovieAPI = async () => {
   const datatorender = await data.json();
   return datatorender;
 };
-const getPremierMovie = async (page) => {
-  page = 1;
+const getUpcomingMovie = async (url, page = 1) => {
   const data = await fetch(
-    `../../Controller/Movie/MoiveService.php?action=getPremieredMovies&page=${page}`,
-    {
-      method: "GET",
-    }
-  );
-  const datatorender = await data.json();
-  return datatorender;
-};
-const getUpcomingMovie = async (page) => {
-  page = 1;
-  const data = await fetch(
-    `../../Controller/Movie/MoiveService.php?action=getUpcomingMovies&page=${page}`,
+    `${url}/Controller/Movie/MoiveService.php?action=getUpcomingMovies&page=${page}`,
     {
       method: "GET",
     }
@@ -34,7 +33,7 @@ const getGenreID = async (page, genreid) => {
   page = 1;
   genreid = 1;
   const data = await fetch(
-    `../../Controller/Movie/MoiveService.php?action=getMoiveByGenres&page=${page}&genreid=${genreid}`,
+    `${url}/Controller/Movie/MoiveService.php?action=getMoiveByGenres&page=${page}&genreid=${genreid}`,
     {
       method: "GET",
     }
@@ -42,4 +41,4 @@ const getGenreID = async (page, genreid) => {
   const datatorender = await data.json();
   return datatorender;
 };
-getHotMovieAPI();
+export { getHotMovieAPI, getPremierMovie, getUpcomingMovie, getGenreID };
