707ed3bc63e0467691ace5a8a7348c30cc2bf709
Ngo Chi Cuong <saofaixoan5464@gmail.com>
2023-04-16 17:07:33 +0700

    Fixing API

--- View/API/GenreAPI.js
+++ View/API/GenreAPI.js
@@ -1,11 +1,9 @@
 const getAllGenre = async (url = "../..", page = 1) => {
   const urls = `${url}/Controller/Genre/ajax.php?action=getAll&page=${page}`;
-  console.log(urls);
   const data = await fetch(urls, {
     method: "GET",
   });
   const datatorender = await data.json();
-  console.log(datatorender);
   return datatorender;
 };
 export { getAllGenre };
