a932dde25767f7da6f89cd0742f4adc54f802578
Ngo Chi Cuong <saofaixoan5464@gmail.com>
2023-04-21 17:10:08 +0700

    Adding Booking Func 80%

--- View/API/ShowTimeAPI.js
+++ View/API/ShowTimeAPI.js
@@ -1,6 +1,6 @@
-const getShowTimeByID = async (url, movieid = 1, date = 16) => {
+const getShowTimeById = async (url, ShowtimeID = 1, date = 16) => {
   const data = await fetch(
-    `${url}/Controller/Showtime/ajax.php?action=getAllShowTimesByMovie&movieid=${movieid}&date=${date}`,
+    `${url}/Controller/Showtime/ajax.php?action=getShowTimeById&id=${ShowtimeID}`,
     {
       method: "GET",
     }
@@ -61,7 +61,6 @@ const getAllShowtimeByMovieID = async (
   const datatorender = await data.json();
   return datatorender;
 };
-
 const updateShowTime = async (
   url = "../..",
   Price,
@@ -137,7 +136,7 @@ const getShowTimeByDateAndTheater = async (
   return datatorender;
 };
 export {
-  getShowTimeByID,
+  getShowTimeById,
   getAllShowTimesByDate,
   getShowTimeByDateAndGenre,
   getShowTimeByMovieandTheater,
