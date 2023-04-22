ac35599620d08966e561d0c614b242615b2519d5
Ngo Chi Cuong <saofaixoan5464@gmail.com>
2023-04-22 17:00:41 +0700

    Change Currency Format

--- View/AdminPage/ComboControl/script.js
+++ View/AdminPage/ComboControl/script.js
@@ -1,7 +1,4 @@
-import{
-    getAllMenu,
-    addMenu,
-} from "../../API/MenuAPI.js";
+import { getAllMenu, addMenu } from "../../API/MenuAPI.js";
 let allData = [];
 let currentData = [];
 let table = $("#table-content").DataTable({
@@ -26,108 +23,116 @@ let table = $("#table-content").DataTable({
 });
 $("#table-content_filter").hide();
 $(document).ready(() => {
-    // table.on("select", function (e, dt, type, indexes) {
-    //   if (type === "row") {
-    //     var data = table.rows(indexes).data();
-    //     fillEditData(data[0][0]);
-    //   }
-    // });
-    $(".item-choosing-block").click(function () {
-      $(".item-choosing-block .divider-mini").remove();
-      $(this).append("<div class=divider-mini></div>");
-    });
-    $(".all-combo").click(() => loadAllMenu().then(() => showData()));
-    $("#btn-search").click(() => {
-      let query = $(".input-place input").val().trim().toUpperCase();
-      currentData = allData.filter(
-        (element) =>
-          element.ItemID.search(query) != -1
-      );
-      $(".item-choosing-block .divider-mini").remove();
-      $(".search-result").parent().append("<div class=divider-mini></div>");
-      showData();
-    });
-    $("#btn-add").click(() => {
-        let ItemID = $("#ModalAddUser #ItemID").val().trim();
-        let Name = $("#ModalAddUser #Name").val().trim();
-        let Price = $("#ModalAddUser #Price").val().trim();
-        let status = $("#ModalAddUser #status").val();
-        let imageFiles = $("#ModalAddUser #image")[0].files;
-        if (!imageFiles) return;
-        $("#ModalAddUser #image")[0]
-          .files.convertAllToBase64(/\.(png|jpeg|jpg|gif)$/i)
-          .then(function (res) {
-            listImage.push(
-              ...res.map((e) => {
-                return { file: e.result, type: 2 };
-              })
-            );
-            console.log(listImage);
-            // addMenu(
-            //   "../../..",
-            //   Name, 
-            //   listImage, 
-            //   Price,
-            //   status 
-            // ).then((res) => {
-            //   if (res.success == false)
-            //     $("#ModalAddUser .message")
-            //       .text("Thêm thất bại")
-            //       .removeClass("success");
-            //   else
-            //     $("#ModalAddUser .message")
-            //       .text("Thêm thành công")
-            //       .addClass("success");
-            // });
-          });
-    })
-    loadAllMenu().then(() => showData());   
+  // table.on("select", function (e, dt, type, indexes) {
+  //   if (type === "row") {
+  //     var data = table.rows(indexes).data();
+  //     fillEditData(data[0][0]);
+  //   }
+  // });
+  $(".item-choosing-block").click(function () {
+    $(".item-choosing-block .divider-mini").remove();
+    $(this).append("<div class=divider-mini></div>");
+  });
+  $(".all-combo").click(() => loadAllMenu().then(() => showData()));
+  $("#btn-search").click(() => {
+    let query = $(".input-place input").val().trim().toUpperCase();
+    currentData = allData.filter(
+      (element) => element.ItemID.search(query) != -1
+    );
+    $(".item-choosing-block .divider-mini").remove();
+    $(".search-result").parent().append("<div class=divider-mini></div>");
+    showData();
+  });
+  $("#btn-add").click(() => {
+    let ItemID = $("#ModalAddUser #ItemID").val().trim();
+    let Name = $("#ModalAddUser #Name").val().trim();
+    let Price = $("#ModalAddUser #Price").val().trim();
+    let status = $("#ModalAddUser #status").val();
+    let imageFiles = $("#ModalAddUser #image")[0].files;
+    if (!imageFiles) return;
+    $("#ModalAddUser #image")[0]
+      .files.convertAllToBase64(/\.(png|jpeg|jpg|gif)$/i)
+      .then(function (res) {
+        listImage.push(
+          ...res.map((e) => {
+            return { file: e.result, type: 2 };
+          })
+        );
+        console.log(listImage);
+        // addMenu(
+        //   "../../..",
+        //   Name,
+        //   listImage,
+        //   Price,
+        //   status
+        // ).then((res) => {
+        //   if (res.success == false)
+        //     $("#ModalAddUser .message")
+        //       .text("Thêm thất bại")
+        //       .removeClass("success");
+        //   else
+        //     $("#ModalAddUser .message")
+        //       .text("Thêm thành công")
+        //       .addClass("success");
+        // });
+      });
+  });
+  loadAllMenu().then(() => showData());
 });
 async function loadAllMenu() {
-    currentData = [];
-  
-    let page = 1;
-    let data;
-    do {
-      data = await getAllMenu("../../..", page);
-      currentData.push(...data.menus);
-      page++;
-    } while (data.menus.length != 0);
-    page = 1;
-    allData = [...currentData];
-  }
+  currentData = [];
+
+  let page = 1;
+  let data;
+  do {
+    data = await getAllMenu("../../..", page);
+    currentData.push(...data.menus);
+    page++;
+  } while (data.menus.length != 0);
+  page = 1;
+  allData = [...currentData];
+}
+function toVndCurrencyFormat(number) {
+  const currencyFormat = new Intl.NumberFormat("vi-VN", {
+    style: "currency",
+    currency: "VND",
+    minimumFractionDigits: 0,
+  });
+
+  return currencyFormat.format(number);
+}
 function showData() {
-    table.clear().draw();
-    let data = currentData;
-    let numRow = data.length;
-    for (let i = 0; i < numRow; i++) {
+  table.clear().draw();
+  let data = currentData;
+  let numRow = data.length;
+  for (let i = 0; i < numRow; i++) {
     //   let genreList = [];
     //   data[i].ListGenre.forEach((genre) => {
     //     genreList.push(genre.GenreName);
     //   });
-      table.row
-        .add([
-          data[i].ItemID,
-          data[i].Name,
-          data[i].Price,
-          data[i].ImageURL,
-          data[i].status
-        ])
-        .draw();
-    }
+    table.row
+      .add([
+        data[i].ItemID,
+        data[i].Name,
+        toVndCurrencyFormat(data[i].Price),
+        data[i].ImageURL,
+        data[i].status,
+      ])
+      .draw();
   }
-  File.prototype.convertToBase64 = function () {
-    return new Promise(
-      function (resolve, reject) {
-        var reader = new FileReader();
-        reader.onloadend = function (e) {
-          resolve({
-            fileName: this.name,
-            result: e.target.result,
-            error: e.target.error,
-          });
-        };
-        reader.readAsDataURL(this);
-      }.bind(this)
-    );
-  };
+}
+File.prototype.convertToBase64 = function () {
+  return new Promise(
+    function (resolve, reject) {
+      var reader = new FileReader();
+      reader.onloadend = function (e) {
+        resolve({
+          fileName: this.name,
+          result: e.target.result,
+          error: e.target.error,
+        });
+      };
+      reader.readAsDataURL(this);
+    }.bind(this)
+  );
+};
