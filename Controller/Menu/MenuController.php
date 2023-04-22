22b79c6239f3b1363e4b68d1a25354b601eef0c5
Huỳnh Nhật Linh <92385756+LinhHNL@users.noreply.github.com>
2023-04-22 16:47:11 +0700

    fix menu update

--- Controller/Menu/MenuController.php
+++ Controller/Menu/MenuController.php
@@ -3,9 +3,7 @@ require_once '../../model/model/MenuModel.php';
 require_once '../../model/entity/Menu.php';
 class MenuController {
     function getAllMenus($page) {
-
         return (new MenuModel())->getAllMenu($page);
-   
     }
     function getMenuById($id) {
         return (new MenuModel())->getMenuByID($id);
@@ -18,11 +16,20 @@ class MenuController {
     function addMenu($data){
       
         $Name = $data['Name'];
-         $ImageURL =  $data['ImageURL'];
+         
          $Price =  $data['Price'];
          $status =  $data['status'];
-   
-        return (new MenuModel())->addMenu(new Menu($Name, $ImageURL, $Price ,$status ));
+         $url_image = $data['file'];
+       
+         $base64 = str_replace('data:application/octet-stream;base64,', '', $url_image);
+         $file = base64_decode($base64);
+         $filename = 'images/img/'. uniqid() . '.jpg'; // generate a unique filename
+         if(file_put_contents("../../".$filename, $file)) {
+             $responses[] = ['status' => 'success', 'message' => 'File saved successfully.'];
+         } else {
+             return  ['status' => 'error', 'message' => 'Error saving file.'];
+         }
+        return (new MenuModel())->addMenu(new Menu($Name, $filename, $Price ,$status ));
     }
     function removeMenu($id){
         return (new MenuModel())->deleteMenu($id);
